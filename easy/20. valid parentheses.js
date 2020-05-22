31f051642f3ace1b86e5b05d9ea2d73c5aa2b618
Harsh Shah <harsh.shah000@gmail.com>
2020-05-22 12:33:12 -0400

    Valid Parentheses

---
+++ easy/20. valid parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var map = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+};
+var isValid = function (s) {
+  var stack = [];
+
+  for (var i = 0; i < s.length; i++) {
+    var el = s[i];
+
+    if (map[el]) {
+      stack.push(map[el]);
+    } else {
+      if (el !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
